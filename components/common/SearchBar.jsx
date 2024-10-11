798ce0e4c33eca022813b3183d25ecee77c90ebc
davm07 <diegovasquezmejia1999@gmail.com>
2024-10-11 23:33:31 +0200

    feat: add avatar, wishlist, cart buttons to display proper information

--- components/common/SearchBar.jsx
+++ components/common/SearchBar.jsx
@@ -3,9 +3,9 @@ import { Button } from '@/components/ui/button';
 
 export default function SearchBar() {
     return (
-    <div className="flex w-full max-w-sm items-center space-x-2">
+    <div className="flex w-full items-center space-x-2 md:max-w-sm">
       <Input type="text" placeholder="Search products..." />
-      <Button type="submit">Search</Button>
+      <Button type="submit" className="md:hover:bg-secondary1-800 md:hover:text-text-50">Search</Button>
     </div>
     )
 }
--- components/layout/HeaderMenu.jsx
+++ components/layout/HeaderMenu.jsx
@@ -1,9 +1,11 @@
 import SearchBar from '@/components/common/SearchBar';
+import ToolBar from '@/components/common/ToolBar';
 
 export default function HeaderMenu() {
     return (
-        <div className="flex flex-col items-center md:flex-row justify-between">
+        <div className="flex flex-col-reverse m-4 gap-3 md:mx-6 md:flex-row md:justify-between">
             <SearchBar/>
+            <ToolBar/>
         </div>
     )
 }
