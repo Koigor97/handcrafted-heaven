9411579e2b310d4da9a8556c078bf8dcff563d28
sheldonu <uch20002@byui.edu>
2024-10-15 16:44:28 -0700

    products added to home page

--- app/page.jsx
+++ app/page.jsx
@@ -3,16 +3,27 @@ import HeroMain from '@/components/layout/HeroMain';
 import BannerArrivals from '@/components/layout/BannerArrivals';
 import ArtisansSection from '@/components/layout/ArtistSection';
 import HeaderMenu from '@/components/layout/HeaderMenu';
+import ProductGrid from '@/components/layout/ProductGrid';
+import { getAllProducts } from '@/services/productService';
+
+
 
 export default async function Home() {
   // const products = await getProductById("e41bc845-8e19-403d-96eb-f9aa691df447");
   // console.log(products);
+  const products = await getAllProducts();
+
 
   return (
     <>
     <HeaderMenu/>
     <HeroMain />
     <BannerArrivals />
+    <div className="my-8">
+        <h2 className="text-center text-4xl font-extrabold text-gray-800 mb-2">Featured Products</h2>
+        <p className="text-center text-lg text-gray-500">Check out our latest arrivals</p>
+        <ProductGrid products={products} />
+    </div>
     <ArtisansSection />
     </>
   );
