3e02753722a70dcabe8fd7c923dcdb5d102646d3
Koigor97 <turaysamuel101@yahoo.com>
2024-10-18 17:32:23 +0200

    create the login page

---
+++ app/auth/login/page.jsx
@@ -0,0 +1,8 @@
+import LoginForm from "@/components/auth/LoginForm";
+
+export const description =
+  "A login form with email and password. There's an option to login with Google and a link to sign up if you don't have an account.";
+
+export default function LoginFormPage() {
+  return <LoginForm />;
+}
