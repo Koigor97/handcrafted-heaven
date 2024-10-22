d2a451302c8947ddccc3b24c4c246bb764b1090a
Koigor97 <turaysamuel101@yahoo.com>
2024-10-22 21:46:21 +0200

    create TotalSales and revenue chart layout section

---
+++ components/layout/SalesRevenueChart.jsx
@@ -0,0 +1,11 @@
+import { TotalSalesRevenueChart } from "../common/TotalsalesRevenueChart";
+
+function SalesRevenueChartSection() {
+  return (
+    <div>
+      <TotalSalesRevenueChart />
+    </div>
+  );
+}
+
+export default SalesRevenueChartSection;
