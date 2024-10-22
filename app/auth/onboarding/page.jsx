a8af741fb5064a99aed39df6f882b53d9817dd2b
Diego Vasquez <122320757+davm07@users.noreply.github.com>
2024-10-23 00:36:57 +0200

    fix: rename OnboardingForm to ArtisanOnBoardingForm

--- app/auth/onboarding/page.jsx
+++ app/auth/onboarding/page.jsx
@@ -1,5 +1,5 @@
 import { artisanAccountAction } from "@/utils/authAction";
-import ArtisanOnboardingForm from "@/components/onboarding/OnBoardingForm";
+import ArtisanOnboardingForm from "@/components/onboarding/ArtisanOnBoardingForm";
 
 export const description =
   "A sign up form with first name, last name, email and password inside a card. There's an option to sign up with Google and a link to login if you already have an account";
