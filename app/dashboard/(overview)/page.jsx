07df58265f21c92daa63d7a450df7558a321755f
Koigor97 <turaysamuel101@yahoo.com>
2024-10-29 10:26:46 +0200

    add async to Dashboard page

--- app/dashboard/(overview)/page.jsx
+++ app/dashboard/(overview)/page.jsx
@@ -1,9 +1,8 @@
 import OrdersTableArtisan from "@/components/common/OrdersTableArtisan";
-
 import OverviewCardLayout from "@/components/layout/OverviewCardLayout";
 import SalesRevenueChartSection from "@/components/layout/SalesRevenueChart";
 
-function DashboardPage() {
+async function DashboardPage() {
   return (
     <div className="grid gap-5">
       <OverviewCardLayout />
