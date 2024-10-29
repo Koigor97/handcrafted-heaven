8277fcaf65c26c2627bde8787fc611d1524f93e7
Koigor97 <turaysamuel101@yahoo.com>
2024-10-29 10:25:34 +0200

    create loading skeleton for dashboard overview page

--- app/dashboard/(overview)/loading.jsx
+++ app/dashboard/(overview)/loading.jsx
@@ -1,7 +1,28 @@
+import { Skeleton } from "@/components/ui/skeleton";
+
 function OverviewLoadingSkeleton() {
   return (
-    <div>
-      <p>Loading...</p>
+    <div className="grid gap-5">
+      <div className="flex gap-3 justify-between">
+        <Skeleton className="bg-accent1-100 h-[25px] w-full" />
+        <Skeleton className="bg-accent1-100 h-[25px] w-full" />
+      </div>
+      <div className="flex gap-3 justify-between">
+        <Skeleton className="bg-accent1-100 h-[25px] w-full" />
+        <Skeleton className="bg-accent1-100 h-[25px] w-full" />
+      </div>
+      <div className="grid gap-3 sm:grid-cols-2 md:grid-cols-4">
+        <Skeleton className="bg-accent1-100 h-[125px] w-full" />
+        <Skeleton className="bg-accent1-100 h-[125px] w-full" />
+        <Skeleton className="bg-accent1-100 h-[125px] w-full" />
+        <Skeleton className="bg-accent1-100 h-[125px] w-full" />
+      </div>
+      <div>
+        <Skeleton className="bg-accent1-100 w-full h-[500px]" />
+      </div>
+      <div>
+        <Skeleton className="bg-accent1-100 w-full h-[500px]" />
+      </div>
     </div>
   );
 }
