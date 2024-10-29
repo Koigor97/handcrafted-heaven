0831ef48e434dd301b2ad9cb60227d23bd3295d4
Koigor97 <turaysamuel101@yahoo.com>
2024-10-29 10:31:11 +0200

    add search icon component

--- components/common/SearchBar.jsx
+++ components/common/SearchBar.jsx
@@ -1,35 +1,43 @@
 "use client";
 
-import { Input } from '@/components/ui/input';
-import { Button } from '@/components/ui/button';
-import { useRouter, usePathname, useSearchParams } from 'next/navigation';
-import { useDebouncedCallback } from 'use-debounce';
+import { Search } from "lucide-react";
+
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { useRouter, usePathname, useSearchParams } from "next/navigation";
+import { useDebouncedCallback } from "use-debounce";
 
 export default function SearchBar() {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
 
-
   const handleSearch = useDebouncedCallback((value) => {
     const params = new URLSearchParams(searchParams);
     if (value) {
-      params.set('query', value);
+      params.set("query", value);
     } else {
-      params.delete('query');
+      params.delete("query");
     }
 
     replace(`${pathname}?${params.toString()}`);
-  }, 400)
+  }, 400);
 
-    return (
+  return (
     <div className="flex w-full items-center space-x-2 md:max-w-sm">
-      <Input type="text"
-      placeholder="Search products..."
-      defaultValue={searchParams.get('query')?.toString()}
-      onChange = {(e) => handleSearch(e.target.value)}
+      <Input
+        type="text"
+        placeholder="Search products..."
+        defaultValue={searchParams.get("query")?.toString()}
+        onChange={(e) => handleSearch(e.target.value)}
       />
-      <Button type="submit" className="md:hover:bg-secondary1-800 md:hover:text-text-50">Search</Button>
+      <Button
+        type="submit"
+        className="flex items-center md:hover:bg-secondary1-800 md:hover:text-text-50 py-2 px-3"
+      >
+        <Search className="w-5 h-5 md:hidden" />
+        <span className="hidden md:flex">Search</span>
+      </Button>
     </div>
-    )
+  );
 }
