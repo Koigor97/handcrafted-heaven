070759ef856bc066487ccd5e6ab2e19445ffd342
Koigor97 <turaysamuel101@yahoo.com>
2024-10-29 10:27:27 +0200

    add product modal and artisan product card grid component

--- app/dashboard/products/page.jsx
+++ app/dashboard/products/page.jsx
@@ -1,9 +1,15 @@
 import SearchBar from "@/components/common/SearchBar";
+import ArtisanAddProductsModal from "@/components/common/ArtisanAddProducts";
+import DashboardProductCard from "@/components/common/DashboardProductCard";
 
 function ProductListPage() {
   return (
-    <section>
-      <SearchBar />
+    <section className="grid gap-4 md:gap-7 mt-3">
+      <div className="flex gap-2 justify-between items-center md:flex-row">
+        <SearchBar />
+        <ArtisanAddProductsModal />
+      </div>
+      <DashboardProductCard />
     </section>
   );
 }
